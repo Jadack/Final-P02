3f7f9996a3928557011f42a0fd32ebcbbf781715
jpacheco-oriontek <jpacheco@oriontek.do>
2019-05-20 13:58:52 -0400

    Add project files.

---
+++ Student Register/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Student_Register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
