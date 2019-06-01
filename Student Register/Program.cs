54fda6c96ee8dd5c2e4169dd48f08d44673a10ef
Jadack <jackdamiolixp@gmail.com>
2019-06-01 13:20:12 -0400

    Clases nuevas agregadas y Master duplicado removido.

--- Student Register/Program.cs
+++ Student Register/Program.cs
@@ -5,7 +5,7 @@ namespace Student_Register
 {
     class Program
     {
-        static List<Student> Students = new List<Student>();
+        static StudentList Students = new StudentList();
 
         static void Menu()
         {
@@ -52,22 +52,8 @@ namespace Student_Register
         static void ListStudent()
         {
             Console.Clear();
-            if (Students.Count == 0)
-            {
-                Console.WriteLine("\n ******************************************");
-                Console.Write("\t No posee datos actualmente");
-                Console.WriteLine("\n ******************************************");
-            } else
-            {
-                string mensaje = "{0, -5}{1, -10}{2,-30}{3,-15}{4,-15}{5,-10}{6,-30}{7,-10}";
-                Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:", "Cédula:", "Celular:", "Carrera:", "Email:", "Fecha de Nacimiento:" + "\n");
 
-                for (var s = 0; s < Students.Count; s++ )
-                {
-                    Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName, Students[s].DocumentNumber, Students[s].Phone, Students[s].Career, Students[s].Email, Students[s].BirthDate.ToString("dd/MM/yyyy"));
-                }
-
-            }
+            Students.ListStudents();
 
             Console.WriteLine("\n\n ¿Qué desea hacer? \n");
             Console.WriteLine("1- Mantenerse en Lista");
@@ -87,35 +73,13 @@ namespace Student_Register
         }
         static void AddStudent()
         {
-            int day, month, year;
             int addOption = 1;
-            Student newStudent = new Student();
 
             Console.Clear();
 
             while (addOption == 1)
             {
-                Console.WriteLine("\n Ingrese los datos del estudiante \n");
-                Console.Write("ID: ");
-                newStudent.Id = Console.ReadLine();
-                Console.Write("Nombre Completo: ");
-                newStudent.FullName = Console.ReadLine();
-                Console.Write("Cédula: ");
-                newStudent.DocumentNumber = Console.ReadLine();
-                Console.Write("Carrera: ");
-                newStudent.Career = Console.ReadLine();
-                Console.Write("Teléfono: ");
-                newStudent.Phone = Console.ReadLine();
-                Console.Write("Email: ");
-                newStudent.Email = Console.ReadLine();
-                Console.Write("Día de Nacimiento: ");
-                day = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Mes de Nacimiento: ");
-                month = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Año de Nacimiento: ");
-                year = Convert.ToInt32(Console.ReadLine());
-                newStudent.BirthDate = new DateTime(year, month, day);
-                Students.Add(newStudent);
+                Students.AddStudent();
 
                 Console.WriteLine("\n ¿Qué desea hacer? \n");
                 Console.WriteLine("1- Agregar otro estudiante");
@@ -135,52 +99,11 @@ namespace Student_Register
         }
         static void EditStudent()
         {
-            int day, month, year;
             int editOption = 1;
             Console.Clear();
             while (editOption == 1)
             {
-                Console.WriteLine("\n Editar estudiantes \n");
-                if (Students.Count == 0)
-                {
-                    Console.WriteLine("\n ******************************************");
-                    Console.Write("\t No posee datos para editar");
-                    Console.WriteLine("\n ******************************************");
-                }
-                else
-                {
-                    string mensaje = "{0, -5}{1, -10}{2,-30}";
-                    Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:" + "\n");
-
-                    for (var s = 0; s < Students.Count; s++)
-                    {
-                        Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName);
-                    }
-
-                    Console.Write("\n Escriba la posición del estudiante que desea editar: ");
-                    int id = Convert.ToInt32(Console.ReadLine()); ;
-                    id = id - 1;
-                    Console.WriteLine("\n Ingrese los nuevos datos del estudiante \n");
-                    Console.Write("ID: ");
-                    Students[id].Id = Console.ReadLine();
-                    Console.Write("Nombre Completo: ");
-                    Students[id].FullName = Console.ReadLine();
-                    Console.Write("Cédula: ");
-                    Students[id].DocumentNumber = Console.ReadLine();
-                    Console.Write("Carrera: ");
-                    Students[id].Career = Console.ReadLine();
-                    Console.Write("Teléfono: ");
-                    Students[id].Phone = Console.ReadLine();
-                    Console.Write("Email: ");
-                    Students[id].Email = Console.ReadLine();
-                    Console.Write("Día de Nacimiento: ");
-                    day = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Mes de Nacimiento: ");
-                    month = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Año de Nacimiento: ");
-                    year = Convert.ToInt32(Console.ReadLine());
-                    Students[id].BirthDate = new DateTime(year, month, day);
-                }
+                Students.EditStudent();
 
                 Console.WriteLine("\n ¿Qué desea hacer? \n");
                 Console.WriteLine("1- Editar otro estudiante");
@@ -205,27 +128,7 @@ namespace Student_Register
             Console.Clear();
             while (deleteOption == 1)
             {
-                Console.WriteLine("\n Eliminar estudiantes \n");
-                if (Students.Count == 0)
-                {
-                    Console.WriteLine("\n ******************************************");
-                    Console.Write("\t No posee datos para borrar");
-                    Console.WriteLine("\n ******************************************");
-                }
-                else
-                {
-                    string mensaje = "{0, -5}{1, -10}{2,-30}";
-                    Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:" + "\n");
-
-                    for (var s = 0; s < Students.Count; s++)
-                    {
-                        Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName);
-                    }
-
-                    Console.Write("\n Escriba la posición del estudiante que desea eliminar: ");
-                    int id = Convert.ToInt32(Console.ReadLine()); ;
-                    Students.RemoveAt(id - 1);
-                }
+                Students.DeleteStudent();
 
                 Console.WriteLine("\n ¿Qué desea hacer? \n");
                 Console.WriteLine("1- Eliminar otro estudiante");
@@ -250,68 +153,8 @@ namespace Student_Register
             Console.Clear();
             while (searchOption == 1)
             {
-                Console.WriteLine("\n ¿Por qué opción desea buscar? \n");
-                Console.WriteLine("1- Por ID");
-                Console.WriteLine("2- Por Nombre");
-                Console.WriteLine("3- Por Carrera");
-                Console.Write("Elija una opción: ");
-                int sOpt = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Digite el valor de busqueda: ");
-                string searchValue = Console.ReadLine();
-                bool flag = true;
-                bool flagHeader = true;
-                for (var s = 0; s < Students.Count; s++)
-                {
-                    switch (sOpt)
-                    {
-                        case 1:
-                            if (Students[s].Id.Contains(searchValue))
-                            {
-                                flag = false;
-                                string mensaje = "{0, -5}{1, -10}{2,-30}{3,-10}";
-                                if (flagHeader)
-                                {
-                                    Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:", "Carrera:" + "\n");
-                                    flagHeader = false;
-                                }
-                                
-                                Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName, Students[s].Career);
-                            }
-                            break;
-                        case 2:
-                            if (Students[s].FullName.Contains(searchValue))
-                            {
-                                flag = false;
-                                string mensaje = "{0, -5}{1, -10}{2,-30}{3,-10}";
-                                if (flagHeader)
-                                {
-                                    Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:", "Carrera:" + "\n");
-                                    flagHeader = false;
-                                }
-                                Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName, Students[s].Career);
-                            }
-                            break;
-                        case 3:
-                            if (Students[s].Career.Contains(searchValue))
-                            {
-                                flag = false;
-                                string mensaje = "{0, -5}{1, -10}{2,-30}{3,-10}";
-                                if (flagHeader)
-                                {
-                                    Console.WriteLine("\n" + "\t" + mensaje, " # ", "ID:", "Nombre Completo:", "Carrera:" + "\n");
-                                    flagHeader = false;
-                                }
-                                Console.WriteLine("\t" + mensaje, (s + 1), Students[s].Id, Students[s].FullName, Students[s].Career);
-                            }
-                            break;
-                    }
-                }
-                if (flag)
-                {
-                    Console.WriteLine("\n ******************************************");
-                    Console.Write("\t No se encontraron registros");
-                    Console.WriteLine("\n ******************************************");
-                }
+                Students.SearchStudent();
+
                 Console.WriteLine("\n ¿Qué desea hacer? \n");
                 Console.WriteLine("1- Buscar otro estudiante");
                 Console.WriteLine("2- Menú Principal");
@@ -351,7 +194,7 @@ namespace Student_Register
             }
 
         }
-        static void Main(string[] args)
+        static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Menu();
--- Student Register/Student.cs
+++ Student Register/Student.cs
@@ -1,17 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Student_Register
 {
     class Student
     {
-        public string Id { get; set; }
-        public string FullName { get; set; }
-        public string DocumentNumber { get; set; }
-        public string Career { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
+        private int id;
+        private string fullName, documentNumber, career, phone, email;
+        private List<int> birthDateList;
+        private static string mensajeFullList = "{0, -10}{1,-30}{2,-15}{3,-15}{4,-10}{5,-30}{6,-10}";
+
+
+        //metodos Get
+        public int Id { get { return this.id; } }
+        public string FullName { get { return this.fullName; } }
+        public string DocumentNumber { get { return this.documentNumber; } }
+        public string Career { get { return this.career; } }
+        public string Phone { get { return this.phone; } }
+        public string Email { get { return this.email; } }
+        public static string MensajeFullList { get { return Student.mensajeFullList; } }
+
+        public DateTime BirthDate()
+        {
+            return new DateTime(this.birthDateList[2], this.birthDateList[1], this.birthDateList[0]);
+        }
+        public string BirthDateString()
+        {
+            return Convert.ToString(this.birthDateList[0]) + "/" + Convert.ToString(this.birthDateList[1]) + "/" + Convert.ToString(this.birthDateList[2]);
+        }
+
+        //metodos Set
+        public void SetId(int newID)
+        {
+            this.id = newID;
+        }
+        public void SetFullName()
+        {
+            Console.Write("Nombre Completo: ");
+            this.fullName = Console.ReadLine();
+        }
+        public void SetDocumentNumber()
+        {
+            Console.Write("Cédula: ");
+            this.documentNumber = Console.ReadLine();
+        }
+        public void SetCareer()
+        {
+            Console.Write("Carrera: ");
+            this.career = Console.ReadLine();
+        }
+        public void SetPhone()
+        {
+            Console.Write("Teléfono: ");
+            this.phone = Console.ReadLine();
+        }
+        public void SetEmail()
+        {
+            Console.Write("Email: ");
+            this.email = Console.ReadLine();
+        }
+        public void SetBirthDate()
+        {
+            Console.Write("Fecha de nacimiento (dd/mm/yyyy): ");
+            string captureData = Console.ReadLine() + "$";
+            List<int> dayMonthYear = new List<int>() { 0, 0, 0 };
+            int position = 0;
+            string numbers = "";
+            
+            foreach (var number in captureData)
+            {
+                if (number == '/')
+                {
+                    dayMonthYear[position] = Convert.ToInt32(numbers);
+                    numbers = "";
+                    position++;
+                    if (position > dayMonthYear.Count - 1) { break; }
+                }
+                else if (!Char.IsDigit(number) & number != '\n' & number != '/')
+                {
+                    if (numbers != "") { dayMonthYear[position] = Convert.ToInt32(numbers); }
+                    break;
+                }
+                else { numbers += Convert.ToString(number); }
+            }
+            this.birthDateList = dayMonthYear;
+        }
+
+        public void PrintAtributes()
+        {
+
+            Console.WriteLine("\t" + mensajeFullList, this.id, this.fullName, this.documentNumber, this.phone, this.career, this.email, this.BirthDateString());
+            
+        }
+
+        public void SetAllAtributes(int newId)
+        {
+            this.SetId(newId);
+            this.SetFullName();
+            this.SetDocumentNumber();
+            this.SetCareer();
+            this.SetPhone();
+            this.SetEmail();
+            this.SetBirthDate();
+        }
+
     }
 }
